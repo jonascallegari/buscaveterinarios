a65919070042d66ed833da3dc2aa692bea5922bc
PANDA PIX <you@example.com>
2026-02-24 15:32:52 -0300

    Botões Cidades

--- public/scripts/public.js
+++ public/scripts/public.js
@@ -17,4 +17,24 @@ function verClinicasDaCidade() {
     window.location.href = `/${path}`;
 }
 
+async function carregarBotoesCidades() {
+
+    const res = await fetch('/api/cities');
+    const cidades = await res.json();
+
+    const container = document.getElementById("botoesCidades");
+
+    if (!container) return;
+
+    container.innerHTML = cidades.map(c => `
+    <a href="/${c.state.toLowerCase()}/${c.slug}" 
+       class="btn btn-outline-light btn-lg m-1 rounded-5">
+       ${c.name} - ${c.state}
+    </a>
+  `).join("");
+
+}
+
+document.addEventListener("DOMContentLoaded", carregarBotoesCidades);
+
 carregarCidadesPublico();
